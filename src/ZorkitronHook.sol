4cb69c1c094b530b14b6f12bcaac466f788c8b9d
Mark Muskardin <markmuskardin@gmail.com>
2026-05-09 15:39:21 -0700

    Edited foundry.toml by downgrading compiler to 0.8.26, and added ZorkitronHook.sol and IZorkitronRouter.sol

---
+++ src/ZorkitronHook.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {BaseHook} from "uniswap-hooks/base/BaseHook.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {BalanceDeltaLibrary, BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {PoolManager} from "v4-periphery/lib/v4-core/src/PoolManager.sol";
+import {IPoolManager, SwapParams, ModifyLiquidityParams} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {IPositionManager} from "v4-periphery/src/PositionManager.sol";
+import {PositionInfo} from "v4-periphery/src/libraries/PositionInfoLibrary.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {IZorkitronRouter} from "./interfaces/IZorkitronRouter.sol";
+
+contract ZorkitronHook is BaseHook {
+    address zorkitronRouterAddr;
+    mapping(address => PositionInfo info) public collateralDeposited;
+
+	// Initialize BaseHook and ERC20
+    // constructor(
+    //     PoolManager _manager,
+    //     address _zorkitronRouterAddr
+    // ) BaseHook(_manager)  {
+    //     zorkitronRouterAddr = _zorkitronRouterAddr;
+    // }
+
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+
+	// Set up hook permissions to return `true`
+	// for the two hook functions we are using
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: true,
+                afterRemoveLiquidity: true,
+                beforeSwap: false,
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+	// Stub implementation for `afterAddLiquidity`
+	function _afterAddLiquidity(
+        address,
+        PoolKey calldata key,
+        ModifyLiquidityParams calldata,
+        BalanceDelta delta,
+		BalanceDelta,
+        bytes calldata hookData
+    ) internal override onlyPoolManager returns (bytes4, BalanceDelta) {
+        address owner = abi.decode(hookData, (address));
+        // First, check that the pool in question is a pool with ETH as one of the tokens.
+        // If currency0 is address(0), then we know its Native ETH.
+        // Native ETH will always be currency0/address(0).
+        if(!key.currency0.isAddressZero()) return (this.afterAddLiquidity.selector, delta);
+
+        IZorkitronRouter(zorkitronRouterAddr).addLiquidity(owner);
+
+        return (this.afterAddLiquidity.selector, delta);
+    }
+
+// Stub implementation for `afterRemoveLiquidity`
+	function _afterRemoveLiquidity(
+        address,
+        PoolKey calldata key,
+        ModifyLiquidityParams calldata,
+        BalanceDelta delta,
+		BalanceDelta,
+        bytes calldata hookData
+    ) internal override onlyPoolManager returns (bytes4, BalanceDelta) {
+        address owner = abi.decode(hookData, (address));
+
+        IZorkitronRouter(zorkitronRouterAddr).removeLiquidity(owner);
+
+        return (this.afterRemoveLiquidity.selector, delta);
+    }
+
+    // Helper function to generate hookData in the format we care about
+    function getHookData(
+        address owner
+    ) public pure returns (bytes memory) {
+        return abi.encode(owner);
+    }
+}
---
+++ src/interfaces/IZorkitronRouter.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {ZorkitronHook} from "../ZorkitronHook.sol";
+
+interface IZorkitronRouter {
+    function addLiquidity(
+        address owner
+        // address _currency0,
+        // address _currency1
+        // int24 tickLower,
+        // int24 tickUpper,
+        // uint256 liquidity,
+        // uint128 amount0Max,
+        // uint128 amount1Max,
+        // uint256 ethToSend
+    ) external returns (bool success);
+    // function depositCollateral(address owner) external returns (bool success);
+    function removeLiquidity(address owner) external returns (bool success);
+    // function setHookContract(address _hookContractAddr) external;
+    // function initializePool(
+    //     address _currency0,
+    //     address _currency1,
+    //     int24 _tickSpacing,
+    //     uint24 _liquidityProviderFee,
+    //     uint128 amount0Max,
+    //     uint128 amount1Max,
+    //     uint256 ethToSend
+    // ) external returns(bool success);
+}
